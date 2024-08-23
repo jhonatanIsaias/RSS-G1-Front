b9a54875a3e48b7815a68791b545db2dd9f0f8b7
Jhonatan Isaias Dos Santos <jhonatan.santos13@academico.ifs.edu.br>
2024-08-23 20:41:31 -0300

    form dinamic

--- src/app/components/form-register/form-register.component.ts
+++ src/app/components/form-register/form-register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input,Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-form-register',
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './form-register.component.css'
 })
 export class FormRegisterComponent {
+  @Input() formFlag:boolean = false ;
+  @Output() continueClicked = new EventEmitter<void>();
 
+  emitContinueEvent(event : Event) {
+    event.preventDefault();
+    this.continueClicked.emit();
+  }
 }
+
--- src/app/components/register-container/register-container.component.ts
+++ src/app/components/register-container/register-container.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component } from '@angular/core';
 import { FormRegisterComponent } from '../form-register/form-register.component';
+import { NgStyle } from '@angular/common';
+
 
 @Component({
   selector: 'app-register-container',
   standalone: true,
-  imports: [FormRegisterComponent],
+  imports: [FormRegisterComponent, NgStyle],
   templateUrl: './register-container.component.html',
   styleUrl: './register-container.component.css'
 })
-export class RegisterContainerComponent {
+export class RegisterContainerComponent implements AfterContentChecked {
 
+
+  selectFlag:boolean = false;
+  color_select1 = "";
+  color_select2 = "";
+
+
+  colorSelect() {
+    if (this.selectFlag == false) {
+      this.color_select1 = "#920305";
+      this.color_select2 = "#a39999";
+    }
+    else {
+      this.color_select1 = "#a39999";
+      this.color_select2 = "#920305";
+    }
+  }
+  handleContinueClick() {
+    this.selectFlag = true;
+  }
+  ngAfterContentChecked(): void {
+    this.colorSelect();
+  }
 }
+
+
