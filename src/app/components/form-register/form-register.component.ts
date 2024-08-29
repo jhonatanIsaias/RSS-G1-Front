ea750d0d3c96890943a22abd2ac3f7c0bc98d6da
Jhonatan Isaias Dos Santos <jhonatan.santos13@academico.ifs.edu.br>
2024-08-29 08:33:45 -0300

    form part 1 finished

--- src/app/components/form-register/form-register.component.ts
+++ src/app/components/form-register/form-register.component.ts
@@ -3,15 +3,23 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 
 
 @Component({
   selector: 'app-form-register',
   standalone: true,
-  imports: [MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgxMaskDirective,
+    ],
   templateUrl: './form-register.component.html',
-  styleUrl: './form-register.component.css'
+  styleUrl: './form-register.component.css',
+  providers: [provideNgxMask()]
 })
 export class FormRegisterComponent {
   @Input() formFlag:number = 1 ;
